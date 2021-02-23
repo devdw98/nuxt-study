28e44f7307b58a91fde1c1eff0083b8ecb01bd72
dowon <devdw98@gmail.com>
2021-02-24 01:57:19 +0900

    mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,28 @@
-export default function(req, res, next) {
-  if (req.url === "/date") {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ date: new Date() }));
-    return;
+import mongo from "./mongo";
+
+async function handleCall(req, res, next) {
+  if (req.url !== "/date") {
+    return next;
   }
-  next();
+  await mongo.connect();
+  await mongo
+    .db("test-node")
+    .collection("test-nuxt")
+    .replaceOne(
+      {
+        _id: "mydoc"
+      },
+      {
+        date: new Date()
+      },
+      { upsert: true }
+    );
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ date: new Date() }));
+}
+
+export default function(req, res, next) {
+  handleCall(req, res, next).then(n => {
+    if (n) n();
+  });
 }
---
+++ api/mongo.js
@@ -0,0 +1,8 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(
+  process.env.MONGODB_URI || "mongodb://localhost",
+  { useUnifiedTopology: true }
+);
+
+export default client;

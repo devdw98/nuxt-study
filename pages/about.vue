3e4785863c6ead07fd94ddcd31e97bc7807ff54a
dowon <devdw98@gmail.com>
2021-02-23 23:31:31 +0900

    init

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+  //   data() {
+  //     return {
+  //       message: "hello"
+  //     };
+  //   },
+  methods: {
+    sayGoodBye() {
+      //   this.message = "good bye";
+      this.putGreeting("goodBye2");
+    },
+    ...mapMutations({
+      putGreeting: "putGreeting"
+    })
+  },
+  computed: {
+    ...mapState({
+      message: store => store.message
+    })
+  }
+};
+</script>
+
+<template>
+  <div>
+    {{ message }}
+    <button @click="sayGoodBye">good</button>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
